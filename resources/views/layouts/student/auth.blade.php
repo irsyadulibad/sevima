39685cae18f9f1402a8b01404a4ecb7331e25562
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2022-06-25 09:43:13 +0700

    feat: student auth login

---
+++ resources/views/layouts/student/auth.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title ?? config('app.name') }}</title>
+
+    <link rel="stylesheet" href="/assets/volt/volt.css">
+</head>
+<body>
+    <main>
+
+        <!-- Section -->
+        <section class="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
+            <div class="container">
+                @yield('content')
+            </div>
+        </section>
+    </main>
+
+    @include('layouts.student.script')
+</body>
+</html>
---
+++ resources/views/layouts/student/script.blade.php
@@ -0,0 +1,14 @@
+<script src="/assets/volt/jquery/jquery.min.js"></script>
+<script src="/assets/volt/popperjs/popper.min.js"></script>
+<script src="/assets/volt/bootstrap/bootstrap.min.js"></script>
+<script src="/assets/volt/onscreen/on-screen.umd.min.js"></script>
+<script src="/assets/volt/nouislider/nouislider.min.js"></script>
+<script src="/assets/volt/smooth-scroll/smooth-scroll.polyfills.min.js"></script>
+<script src="/assets/volt/chartist/chartist.min.js"></script>
+<script src="/assets/volt/chartist-plugin-tooltips/chartist-plugin-tooltip.min.js"></script>
+<script src="/assets/volt/sweetalert2/sweetalert2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
+<script src="/assets/volt/vanillajs-datepicker/js/datepicker.min.js"></script>
+<script src="/assets/volt/notyf/notyf.min.js"></script>
+<script src="/assets/volt/simplebar/simplebar.min.js"></script>
+<script src="/assets/volt/volt.js"></script>
